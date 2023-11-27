4c3bbba57d9cb3975a8175b61d52065b7702d1e2
unknown <riasat01@outlook.com>
2023-11-27 18:15:27 +0600

    firebase auth added

--- src/authprovider/AuthProvider.jsx
+++ src/authprovider/AuthProvider.jsx
@@ -1,11 +1,11 @@
-// import { GoogleAuthProvider, createUserWithEmailAndPassword, deleteUser, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, deleteUser, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
-// import auth from "../firebase/firebase.config";
 // import useAxiosSecure from "../custom-hooks/useAxiosSecure";
+import auth from "../firebase/firebase.config";
 
 export const UserAuth = createContext({});
-// const googleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -16,67 +16,67 @@ const AuthProvider = ({ children }) => {
     // create user with and password
     const userWithEmail = (email, password) => {
         setLoading(true);
-        // return createUserWithEmailAndPassword(auth, email, password);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
 
     // sign in with email and password
     const loginWithEMail = (email, password) => {
         setLoading(true);
-        // return signInWithEmailAndPassword(auth, email, password);
+        return signInWithEmailAndPassword(auth, email, password);
     }
 
     // continue with google
     const continueWithGoogle = () => {
         setLoading(true);
-        // return signInWithPopup(auth, googleProvider);
+        return signInWithPopup(auth, googleProvider);
     }
 
     // setting user name
     const setUserName = (name, photo) => {
         setLoading(true);
-        // return updateProfile(auth.currentUser, {
-        //     displayName: name,
-        //     photoURL: photo
-        // })
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo
+        })
     }
 
     // delete account
     const deleteAccount = (user) => {
         setLoading(true);
-        // return deleteUser(user)
+        return deleteUser(user);
     }
 
     // sign out
     const logOut = () => {
         setLoading(true);
-        // return signOut(auth);
+        return signOut(auth);
     }
 
     // getting user state
-    // useEffect(() => {
-    //     const unsubscsribe = onAuthStateChanged(auth, (currentUser) => {
-    //         const email = currentUser?.email || user?.email;
-    //         const loggeduser = { email: email };
-    //         console.log(currentUser)
-    //         setUser(currentUser);
-    //         setLoading(false);
-    //         // if user exitst then issue a token
-    //         if (loggeduser.email) {
-    //             axiosSecure.post('/jwt', loggeduser)
-    //                 .then(res => {
-    //                     console.log('token response', res.data);
-    //                 })
-    //         } else {
-    //             axiosSecure.post('/logout', loggeduser)
-    //                 .then(res => {
-    //                     console.log(res.data);
-    //                 })
-    //         }
-    //     });
-    //     return () => {
-    //         unsubscsribe();
-    //     }
-    // }, [])
+    useEffect(() => {
+        const unsubscsribe = onAuthStateChanged(auth, (currentUser) => {
+            const email = currentUser?.email || user?.email;
+            const loggeduser = { email: email };
+            console.log(currentUser)
+            setUser(currentUser);
+            setLoading(false);
+            // if user exitst then issue a token
+            // if (loggeduser.email) {
+            //     axiosSecure.post('/jwt', loggeduser)
+            //         .then(res => {
+            //             console.log('token response', res.data);
+            //         })
+            // } else {
+            //     axiosSecure.post('/logout', loggeduser)
+            //         .then(res => {
+            //             console.log(res.data);
+            //         })
+            // }
+        });
+        return () => {
+            unsubscsribe();
+        }
+    }, [])
 
 
     useEffect(() => {
