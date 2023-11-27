14860844c03647781a1a171fef3ae9661d3bb36b
unknown <riasat01@outlook.com>
2023-11-27 17:56:45 +0600

    login page added

--- src/pages/mainlayout-pages/login-page/login/Login.jsx
+++ src/pages/mainlayout-pages/login-page/login/Login.jsx
@@ -1,10 +1,71 @@
-
+import { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import { FcGoogle } from 'react-icons/fc';
+import { UserAuth } from "../../../../authprovider/AuthProvider";
+import loginBg from '../../../../assets/images/login.jpg'
 
 const Login = () => {
+    const { user, setLoading, loginWithEMail, continueWithGoogle } = useContext(UserAuth);
+    const navigate = useNavigate();
+
+    // where to re route
+    const location = useLocation();
+    // console.log(`State from login ${location?.state}`)
+
+    // sign in user
+    const handleSignIn = (e) => {
+        e?.preventDefault();
+        loginWithEMail(e?.target?.email?.value, e?.target?.password?.value)
+            .then(userCredintial => {
+                console.log(userCredintial.user);
+                swal(`Congratulation ${user?.displayName ? user?.displayName : ``}`, `You have successfully signed in`, `success`)
+                location?.state ? navigate(`${location?.state}`) : navigate(`/`);
+            })
+            .catch(error => {
+                swal(`Error`, error.message, `error`);
+                setLoading(false);
+            })
+    }
+
+    const handlethirdPartySignIn = (callback) => {
+        callback()
+            .then(userCredintial => {
+                // console.log(userCredintial.user);
+                swal(`Congratulation ${userCredintial?.user?.displayName}`, `You have successfully signed in with Google`, `success`)
+                location?.state ? navigate(`${location?.state}`) : navigate(`/`);
+            })
+            .catch(error => {
+                swal(`Error`, error.message, `error`);
+                setLoading(false);
+            })
+    }
     return (
-        <div>
-            
-        </div>
+        <section style={{
+            backgroundImage: `url(${loginBg}), linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))`,
+            backgroundBlendMode: 'overlay',
+            backgroundSize: 'cover',
+            backgroundAttachment: 'fixed'
+        }} className="w-screen h-screen">
+            <section className="w-full md:w-1/2 lg:w-1/3 mx-auto h-screen pt-48 pb-12">
+                <section className="flex justify-center items-center">
+                    <form onSubmit={handleSignIn} className="space-y-6">
+                        <input className="w-full text-white rounded-2xl py-2 px-4 bg-slate-400 bg-opacity-70" type="email" name="eamil" placeholder="Email" id="email" required />
+                        <input className="w-full text-white rounded-2xl py-2 px-4 bg-slate-400 bg-opacity-70" type="password" name="password" placeholder="Password" id="password" required />
+                        <input className="w-full px-5 py-2 rounded-2xl bg-transparent hover:bg-gradient-to-tr from-pink-600 to-pink-400 text-lg font-semibold text-white border-2 hover:border-0 border-pink-500 hover:shadow-[1px_-1px_1rem_0px_pink] " type="submit" value="Login" />
+                        <p className="text-white">New here? Please <Link to='/login/register' className="font-semibold text-transparent bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text border-b-2 border-pink-600">Register</Link></p>
+                    </form>
+                </section>
+                <fieldset className="border-t border-slate-600 my-6">
+                    <legend className="text-center px-1 text-white">Or continue with</legend>
+                </fieldset>
+                <section className="flex justify-center items-center">
+                    <FcGoogle onClick={() => handlethirdPartySignIn(continueWithGoogle)} className="text-4xl w-fit"></FcGoogle>
+                </section>
+            </section>
+
+        </section>
+
     );
 };
 
--- src/routes/main-route/MainRoute.jsx
+++ src/routes/main-route/MainRoute.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../layouts/mainlayout/MainLayout";
 import ErrorPage from "../../pages/error-page/ErrorPage";
 import Home from "../../pages/mainlayout-pages/home/Home";
+import LoginPage from "../../pages/mainlayout-pages/login-page/LoginPage";
+import Login from "../../pages/mainlayout-pages/login-page/login/Login";
+import Register from "../../pages/mainlayout-pages/login-page/register/Register";
 
 
 const MainRoute = createBrowserRouter ([
@@ -13,6 +16,20 @@ const MainRoute = createBrowserRouter ([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <LoginPage></LoginPage>,
+                children: [
+                    {
+                        path: '/login',
+                        element: <Login></Login>
+                    },
+                    {
+                        path: '/login/register',
+                        element: <Register></Register>
+                    }
+                ]
             }
         ]
     }
